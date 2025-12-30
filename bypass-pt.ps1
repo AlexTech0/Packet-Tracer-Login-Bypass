70171c5aed422baf0dd82aa1a640f72979829a8f
altech3 <115791240+altech3@users.noreply.github.com>
2025-12-30 13:37:16 +0100

    Improve code and fix some things

--- bypass-pt.ps1
+++ bypass-pt.ps1
@@ -9,7 +9,7 @@ Tested on:
 #Requires -RunAsAdministrator
 Add-Type -AssemblyName System.Windows.Forms # <-- For the open file dialog box
 
-# Default Packet Tracer folders
+# Default Packet Tracer executable locations
 $default_pt9_path = "C:\Program Files\Cisco Packet Tracer 9.0.0\bin\PacketTracer.exe"
 $default_pt8_path = "C:\Program Files\Cisco Packet Tracer 8.2.2\bin\PacketTracer.exe"
 
@@ -28,6 +28,12 @@ function script_help {
     Write-Host "Arguments:`n-r, -R, --remove`t-->`tRemove Firewall rules (instead of creating them)"
 }
 
+# To press any key to continue
+function press_any_key {
+    Write-Host -NoNewLine 'Press any key to continue...'
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+}
+
 function pt_file_dialog {
     $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
     InitialDirectory = [Environment]::GetFolderPath('ProgramFiles')
@@ -37,22 +43,26 @@ function pt_file_dialog {
 #   Prompt user to locate the .exe file of PT
     Write-Host "Please, locate Cisco Packet Tracer executable (.exe) file manually, using GUI..."; Start-Sleep 1.5
     
-#   To press any key to continue
-    Write-Host -NoNewLine 'Press any key to continue...'
-    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+    press_any_key
 
     $full_path = $FileBrowser.showdialog()
     $script:file_path = $FileBrowser.filename
+    
+#   Make sure that this operation wasn't canceled by user
+    if ( $script:file_path -eq "" ){
+        Write-Host "`nSorry, looks like this operation has been canceled."
+        exit 1
+    }
 }
 
 function locate_packet_tracer {
-#   First try to locate PT in the default installation location
-    if ( test-path $default_pt9_path ) {
-        Write-Host "Detected Packet Tracer 9 in this directory: $default_pt9_path"
+#   First try to locate PT in the default directory
+    if ( Test-Path $default_pt9_path -PathType Leaf ) {
+        Write-Host "Detected Packet Tracer 9 executable: $default_pt9_path"
         Write-Host "Is this the correct one to bypass login? [Y\N]`n"
         $input_prompt = Read-Host -prompt ">>> "
 
-        if ( $input_prompt.ToLower() -eq "y" ){
+        if ( $input_prompt.ToLower() -eq "y" -or $input_prompt.ToLower() -eq "yes" ){
             $script:file_path = $default_pt9_path
         } else {
             Start-Sleep 1
@@ -60,12 +70,12 @@ function locate_packet_tracer {
         }
  
     }
-    elseif ( test-path $default_pt8_path ) {
-        Write-Host "Detected Packet Tracer 8 in this directory: $default_pt9_path"
+    elseif ( Test-Path $default_pt8_path -PathType Leaf ) {
+        Write-Host "Detected Packet Tracer 8 executable: $default_pt8_path"
         Write-Host "Is this the correct one to bypass login? [Y\N]`n"
         $input_prompt = Read-Host -prompt ">>> "
 
-        if ( $input_prompt.ToLower() -eq "y" ){
+        if ( $input_prompt.ToLower() -eq "y" -or $input_prompt.ToLower() -eq "yes" ){
             $script:file_path = $default_pt8_path
         } else {
             Start-Sleep 1
@@ -79,43 +89,43 @@ function locate_packet_tracer {
 
 function add_inbound_rule {
 #   Check if the rule with the same name already exists (just in case)
-    if ( get-netfirewallrule -DisplayName $in_rule_name -ErrorAction SilentlyContinue ) {
+    if ( Get-NetFirewallRule -DisplayName $in_rule_name -ErrorAction SilentlyContinue ) {
         Write-Host "Firewall rule '$in_rule_name' already exists`nShould I delete it?`n"
         Write-Host "Options:`n1: --> delete`n2: --> Skip"
         
         $input_prompt = Read-Host -prompt ">>> "
-        if ( $input_prompt -eq 1 -or $input_prompt -eq "delete" ) {
+        if ( $input_prompt.ToLower() -eq "delete" -or $input_prompt.ToLower() -eq "1" ) {
             Remove-NetFirewallRule -DisplayName $in_rule_name
 #           And create new rule
-            New-NetFirewallRule -DisplayName $in_rule_name -Description "Bypass Login Prompt in Cisco Packet Tracer" -Direction Inbound -Program $file_path -Action Block -Profile "Any"
+            New-NetFirewallRule -DisplayName $in_rule_name -Description "Bypass Login Prompt in Cisco Packet Tracer" -Direction Inbound -Program $script:file_path -Action Block -Profile "Any"
         } else {
 #           Skip
             return
         }
 
     } else {
-        New-NetFirewallRule -DisplayName $in_rule_name -Description "Bypass Login Prompt in Cisco Packet Tracer" -Direction Inbound -Program $file_path -Action Block -Profile "Any"
+        New-NetFirewallRule -DisplayName $in_rule_name -Description "Bypass Login Prompt in Cisco Packet Tracer" -Direction Inbound -Program $script:file_path -Action Block -Profile "Any"
     }
 }
 
 function add_outbound_rule {
     #   Check if the rule with the same name already exists (just in case)
-    if ( get-netfirewallrule -DisplayName $out_rule_name -ErrorAction SilentlyContinue ) {
+    if ( Get-NetFirewallRule -DisplayName $out_rule_name -ErrorAction SilentlyContinue ) {
         Write-Host "Firewall rule '$out_rule_name' already exists`nShould I delete it?`n"
         Write-Host "Options:`n1: --> delete`n2: --> Skip"
         
         $input_prompt = read-host -prompt ">>> "
-        if ( $input_prompt -eq 1 ) {
+        if ( $input_prompt.ToLower() -eq "delete" -or $input_prompt.ToLower() -eq "1" ) {
             Remove-NetFirewallRule -DisplayName $out_rule_name
 #           And add new rule
-            New-NetFirewallRule -DisplayName $out_rule_name -Description "Bypass Login Prompt in Cisco Packet Tracer" -Direction Outbound -Program $file_path -Action Block -Profile "Any"
+            New-NetFirewallRule -DisplayName $out_rule_name -Description "Bypass Login Prompt in Cisco Packet Tracer" -Direction Outbound -Program $script:file_path -Action Block -Profile "Any"
         } else {
 #           Skip
             return
         }
 
     } else {
-        New-NetFirewallRule -DisplayName $out_rule_name -Description "Bypass Login Prompt in Cisco Packet Tracer" -Direction Outbound -Program $file_path -Action Block -Profile "Any"
+        New-NetFirewallRule -DisplayName $out_rule_name -Description "Bypass Login Prompt in Cisco Packet Tracer" -Direction Outbound -Program $script:file_path -Action Block -Profile "Any"
     }
 }
 
@@ -139,7 +149,7 @@ function enable_rules {
 # In case the user needs to remove them, the rules are going to be removed by their DisplayName
 function remove_rules {
 #   Inbound rule
-    if ( get-netfirewallrule -DisplayName $in_rule_name -ErrorAction SilentlyContinue ) {
+    if ( Get-NetFirewallRule -DisplayName $in_rule_name -ErrorAction SilentlyContinue ) {
         Remove-NetFirewallRule -DisplayName $in_rule_name
         Write-Host "Removed rule: $in_rule_name"
     } else {
@@ -147,15 +157,14 @@ function remove_rules {
     }
 
 #   Outbound rule
-    if ( get-netfirewallrule -DisplayName $out_rule_name -ErrorAction SilentlyContinue ) {
+    if ( Get-NetFirewallRule -DisplayName $out_rule_name -ErrorAction SilentlyContinue ) {
         Remove-NetFirewallRule -DisplayName $out_rule_name
         Write-Host "Removed rule: $out_rule_name"
     } else {
-        Write-Host "Rule '$in_rule_name' doesn't exist, so not removing"
+        Write-Host "Rule '$out_rule_name' doesn't exist, so not removing"
     }
     Write-Host ""
     execution_policy_reminder
-
 }
 
 function main {
@@ -170,23 +179,22 @@ function main {
 
     try {
         locate_packet_tracer
-    }
-    catch {
+    } catch {
         Write-Host "Sorry, couldn't locate Packet Tracer"
         exit
     }
+
     try {
         add_inbound_rule
         add_outbound_rule
-    }
-    catch {
+    } catch {
         Write-Host "Sorry, there has been issue/s with adding rules"
         exit
     }
+
     try {
         enable_rules
-    }
-    catch {
+    } catch {
         Write-Host "Sorry, couldn't enable rules"
         exit
     }
